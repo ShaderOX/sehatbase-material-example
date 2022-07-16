3b3a8ec2e6c980f952d1efa86b62ff20e3c819c6
Asadullah Kashif <s.asad18k@gmail.com>
2022-07-16 05:25:42 +0500

    Added tags to blogs card

--- src/components/BlogCard/BlogCard.jsx
+++ src/components/BlogCard/BlogCard.jsx
@@ -42,13 +42,31 @@ const BlogCard = ({ blog }) => {
             </Typography>
           </CardContent>
 
+          <CardContent>
+            <Box sx={{ display: "flex", justifyContent: "space-evenly" }}>
+              {blog.tags.length > 0 &&
+                blog.tags.slice(0, 5).map((tag, idx) => {
+                  return (
+                    <Link key={`${tag}-${idx}`} to={`/blogs?tag=${tag}`}>
+                      <Typography
+                        variant="caption"
+                        sx={{ textDecoration: "underline" }}
+                      >
+                        {`${tag.charAt(0).toUpperCase()}${tag.slice(1)}`}
+                      </Typography>
+                    </Link>
+                  );
+                })}
+            </Box>
+          </CardContent>
+
           <CardActions>
             <Box
               width="100%"
               sx={{ display: "flex", justifyContent: "space-between" }}
             >
               <Button size="small">Add to List</Button>
-              <Link to={`/posts/${blog.id}`}>
+              <Link to={`/blogs/${blog.id}`}>
                 <Button size="small">Learn More</Button>
               </Link>
             </Box>
